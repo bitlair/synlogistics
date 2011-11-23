1259cc74f8339250cdf333c8ca1fec308a1a773d
Jeroen Dekkers <jeroen@dekkers.ch>
2011-11-23 21:34:21 +0100

    Use constance for settings

--- invoicing/models.py
+++ invoicing/models.py
@@ -19,7 +19,8 @@ SynLogistics: Invoice class
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 from django.db import models, transaction as db_trans
-from main.models import BookingPeriod, Settings
+from django.core.exceptions import ImproperlyConfigured
+from main.models import BookingPeriod
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import mm
@@ -29,6 +30,7 @@ from cStringIO import StringIO
 from pyPdf import PdfFileReader, PdfFileWriter
 from os.path import exists
 from settings import STATIC_ROOT
+from constance import config
 import os
 import errno
 
@@ -37,8 +39,8 @@ class Invoice(models.Model):
     customer = models.ForeignKey('main.Relation', related_name='invoices', null=False)
     date = models.DateField(null=False, db_index=True)
     booking_period = models.ForeignKey('main.BookingPeriod', null=False)
-    number = models.IntegerField(null=False, db_index=True)
-    full_invoice_no = models.CharField(max_length=25, db_index=True)
+    number = models.IntegerField(null=False, db_index=True, editable=False)
+    full_invoice_no = models.CharField(max_length=25, db_index=True, editable=False)
     
     class Meta:
         """ Metadata """
@@ -67,13 +69,8 @@ class Invoice(models.Model):
                 break
             self.number += 1
 
-        # We need the settings for the invoice format string
-        settings = Settings.objects.all()
-        assert settings.count() == 1
-        settings = settings[0]
-
         # Create the full invoice number based on the user defined format string
-        self.full_invoice_no = settings.invoice_format_string % {
+        self.full_invoice_no = config.invoice_number_format % {
                 'booking_period': self.booking_period.number,
                 'year': self.date.year,
                 'month': self.date.month,
@@ -103,13 +100,8 @@ class Invoice(models.Model):
 
         # FIXME This function only works for periodic invoices at this time
 
-        # We need the settings for the letterhead paper path
-        settings = Settings.objects.all()
-        assert settings.count() == 1
-        settings = settings[0]
-
         # Verify the watermark PDF exists or bail
-        if not exists(settings.letterhead_paper_path):
+        if not exists(config.letterhead_paper_path):
             raise IOError(errno.ENOENT)
 
         # Read the letterhead paper
