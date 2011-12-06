eab214a6f63c708d4e00ed4559f24a3c2c8b5ab3
Wilco Baan Hofman <wilco@baanhofman.nl>
2011-12-06 17:05:57 +0100

    Make sure the database backend is also installed.

--- settings.dist.py
+++ settings.dist.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'constance',
+    'constance.backends.database',
     'main',
     'accounting',
     'ajax',
