239b39dbef72b512aa717d85f7e064e61df134f8
Jeroen Dekkers <jeroen@dekkers.ch>
2012-02-01 22:47:46 +0100

    Use inline admin for invoice items

--- invoicing/admin.py
+++ invoicing/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Invoice)
+class InvoiceItemInline(admin.StackedInline):
+    model = InvoiceItem
+    extra = 0
+
+class InvoiceAdmin(admin.ModelAdmin):
+    inlines = [InvoiceItemInline]
+
+
+admin.site.register(Invoice, InvoiceAdmin)
 admin.site.register(InvoiceItem)
 admin.site.register(Subscription)
